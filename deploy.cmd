e9e8bf104576c7e77017df2a4079aae81613c6b9
Igor S <Shcheglovitov@avp.ru>
2016-10-07 00:10:30 +0300

    x12

--- deploy.cmd
+++ deploy.cmd
@@ -2,4 +2,6 @@
 echo Building...
 "%MSBUILD_PATH%" TestFuncLib\TestFuncLib.csproj /p:OutDir=%DEPLOYMENT_SOURCE%\TestFunc\bin\
 echo Deploying files...
-xcopy %DEPLOYMENT_SOURCE%\TestFunc\ %DEPLOYMENT_TARGET%\TestFunc\ /e /s /y
+set targetDir=%DEPLOYMENT_TARGET%\TestFunc
+IF not exist %targetDir% (mkdir %targetDir%)
+xcopy %DEPLOYMENT_SOURCE%\TestFunc %targetDir% /e /s /y
